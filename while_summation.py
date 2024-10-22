35ded490df4b7495c3e81fe90c34ed4532d9d256
Ray McDonald <mcdonbri006@alpinesd.org>
2024-10-22 11:16:40 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,9 @@
 #while summation code following directions from Readme
+user_num = int(input("Enter a number: "))
+looped_times = 0
+total = 0
+
+while looped_times <= user_num:
+    print(looped_times)
+    total = total + looped_times
+    looped_times += 1
